20782f8aa15a222fbb18de15a49c5ae65e3db710
akanksha.mahajan <akankshamahajan99@gmail.com>
2020-04-18 18:59:25 +0530

    Student DAO

--- MEAL_ON/src/main/java/com/mealon/MEAL_ON/dao/StudentDAO.java
+++ MEAL_ON/src/main/java/com/mealon/MEAL_ON/dao/StudentDAO.java
@@ -5,5 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 import com.mealon.MEAL_ON.model.Student;
 
 public interface StudentDAO extends CrudRepository<Student, Integer>{
+
 	Student findByName(String name);
+	Student findByMis(Integer mis);
+	
 }
