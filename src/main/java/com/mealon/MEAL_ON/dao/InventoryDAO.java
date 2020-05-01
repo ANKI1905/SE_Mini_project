cf474911b0ef55201a38108f4d6e41e115cf0880
Musadiq <peermusadiq111@gmail.com>
2020-05-01 14:11:56 +0530

    added delete function with inventory_id

--- src/main/java/com/mealon/MEAL_ON/dao/InventoryDAO.java
+++ src/main/java/com/mealon/MEAL_ON/dao/InventoryDAO.java
@@ -9,4 +9,5 @@ import com.mealon.MEAL_ON.model.Inventory;
 public interface InventoryDAO extends CrudRepository<Inventory, Integer>{
 	Inventory findByName(String name);
 	List<Inventory> findAllByMessid(int mess_id);
+	Inventory findByInventoryid(int inventory_id);
 }
