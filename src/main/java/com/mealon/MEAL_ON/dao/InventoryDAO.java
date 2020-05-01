cc6d2412c5ed7c39bc0c1b7f94b169ca23c1f427
Musadiq <peermusadiq111@gmail.com>
2020-05-02 00:03:54 +0530

    removed findByInventoryid(), used findById()

--- src/main/java/com/mealon/MEAL_ON/dao/InventoryDAO.java
+++ src/main/java/com/mealon/MEAL_ON/dao/InventoryDAO.java
@@ -9,5 +9,4 @@ import com.mealon.MEAL_ON.model.Inventory;
 public interface InventoryDAO extends CrudRepository<Inventory, Integer>{
 	Inventory findByName(String name);
 	List<Inventory> findAllByMessid(int mess_id);
-	Inventory findByInventoryid(int inventory_id);
 }
