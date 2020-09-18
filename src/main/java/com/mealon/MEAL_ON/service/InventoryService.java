bc9dd3f0abfae9416565e70f55d0109e91f65c50
Musadiq <peermusadiq111@gmail.com>
2020-09-18 08:53:19 +0530

    corrected some errors to solve test cases

--- src/main/java/com/mealon/MEAL_ON/service/InventoryService.java
+++ src/main/java/com/mealon/MEAL_ON/service/InventoryService.java
@@ -3,7 +3,6 @@ package com.mealon.MEAL_ON.service;
 import java.util.List;
 
 import com.mealon.MEAL_ON.model.Inventory;
-import com.mealon.MEAL_ON.model.MessStaff;
 
 public interface InventoryService {
     Inventory get(int inventory_id);
